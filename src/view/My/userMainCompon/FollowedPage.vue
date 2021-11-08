a6eaacf132017afbb47f9e4a99c64b545129b8b5
xuan-123 <18735495269@163.com>
2021-11-08 10:48:12 +0800

    粉丝页导航

--- src/view/My/userMainCompon/FollowedPage.vue
+++ src/view/My/userMainCompon/FollowedPage.vue
@@ -1,12 +1,21 @@
 <template>
-    <div>
+    <div class="followed">
+        <nav-bar>
+            <div slot="left"><p class="iconfont icon-fanhui" @click="$router.go(-1)"></p></div>
+            <div slot="center">粉丝</div>
+            <div slot="right"></div>
+        </nav-bar>
         粉丝页
     </div>
 </template>
 
 <script>
-export default {
+import NavBar from '../../../components/NavBar/NavBar.vue'
 
+export default {
+    components:{
+        NavBar
+    }
 }
 </script>
 
